34dfb633ba6a1714c64a3dee6a4b37c780569a4d
Ichsan <013ichsanm@gmail.com>
2025-11-07 11:05:04 +0700

    update

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const startServer = async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅ Connected to PostgreSQL");
+    console.log("✅ Connected to PostgreSQL ichsan");
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   } catch (error) {
     console.error("❌ Unable to connect to DB:", error);
